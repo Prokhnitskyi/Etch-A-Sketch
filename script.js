bab0e2f847f9aa15016cfc258993c59a292ac955
andrii.prokhnitskyi <26540483+Prokhnitskyi@users.noreply.github.com>
2023-04-20 00:15:21 +0300

    Add darken and random color logic

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
     const modalButton = document.querySelector('.controls__show-modal');
     const modal = document.querySelector('.size-modal');
     const sizeForm = document.querySelector('.size-form');
+    const darkenCheckbox = document.getElementById('darken');
+    const randomCheckbox = document.getElementById('random-color');
     let mouseDown = false;
 
     function renderGrid(containerElement, gridSize = 16) {
@@ -11,10 +13,11 @@
         for (let i = 0; i < gridSize ** 2; i++) {
             const cell = document.createElement('div');
             cell.classList.add('grid__element');
+            cell.dataset.brightness = '1';
             containerElement.append(cell);
         }
         gridContainer.dataset.size = gridSize.toString();
-        gridContainer.style.fontSize = `${16 / gridSize}rem`
+        gridContainer.style.fontSize = `${ 16 / gridSize }rem`
     }
 
     function resizeGrid(event) {
@@ -25,8 +28,30 @@
     }
 
     function draw(event) {
-        if (!mouseDown || !event.target.classList.contains('grid__element')) return;
-        event.target.style.backgroundColor = `rgb(0 0 0)`;
+        const cell = event.target;
+        const newBrightnessValue = parseFloat(cell.dataset.brightness) - 0.1;
+
+        if (!mouseDown || !cell.classList.contains('grid__element')) return;
+
+        if (darkenCheckbox.checked) {
+            cell.style.filter = `brightness(${newBrightnessValue})`;
+            cell.dataset.brightness = newBrightnessValue.toFixed(2);
+        }
+
+        if (randomCheckbox.checked) {
+            cell.style.backgroundColor = getRandomColor();
+        } else {
+            cell.style.backgroundColor = `rgb(0 0 0)`;
+        }
+    }
+
+    function getRandomColor() {
+        const rgbOptions = {
+            r: Math.random() * 255,
+            g: Math.random() * 255,
+            b: Math.random() * 255
+        };
+        return `rgb(${rgbOptions.r} ${rgbOptions.g} ${rgbOptions.b})`;
     }
 
     gridContainer.addEventListener('mousemove', draw);
